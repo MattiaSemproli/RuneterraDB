fd7813c597be7176ca9ebfb94cabe69485f83e3b
mattia semproli <mattia.semproli2@studio.unibo.it>
2023-05-02 16:42:32 +0200

    Added a method

--- championsUpdate.php
+++ championsUpdate.php
@@ -2,12 +2,23 @@
     require_once 'connect.php';
 
     $conn = new mysqli($servername, $username, $password, $dbname);
+    if (!$conn) {
+        die("Connessione al database fallita: " . mysqli_connect_error());
+    }
 
     $champions = json_decode($json)->data;
-
     $championArray = array();
     
     foreach ($champions as $champion) {
-        array_push($championArray, $champion);
+        $championData = array(
+            'key' => $champion->key,
+            'name' => $champion->name,
+            'title' => $champion->title,
+            'tags' => $champion->tags,
+            'partype' => $champion->partype,
+        );
+        array_push($championArray, $championData);
     }
+
+    mysqli_close($conn);
 ?>
